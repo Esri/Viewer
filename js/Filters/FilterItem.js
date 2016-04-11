f1581169990e576d6c49c3a97b3035ffdc7c2bb7
Horia Tudosie <htudosie@esri.ca>
2016-04-10 20:36:19 -0400

    Fix bug on Remove btn

--- js/Filters/FilterItem.js
+++ js/Filters/FilterItem.js
@@ -41,7 +41,7 @@ define([
         },
 
         _init: function () {
-            this.domNode = domConstruct.create("div", {innerHTML: this.field.fieldName});
+            domConstruct.create("div", {innerHTML: this.field.fieldName}, this.domNode);
             switch(this.field_Type) {
                 case "esriFieldTypeString" :
                     var filterString = new FilterString({map:this.map, layer:this.layer, field:this.field}, this.content);
