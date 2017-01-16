c45ff45b76215674f711986c511ae63b83dc237f
Horia Tudosie <htudosie@esri.ca>
2017-01-16 13:01:49 -0500

    Localized Base Map Labels

---
+++ js/nls/fr/BaseMapLabels.js
@@ -0,0 +1,14 @@
+define({
+    baseMapLabels: ({
+        imagery: "Images",
+        imagery_with_labels: "Images avec étiquettes",
+        streets: "Rues",
+        topographic: "Topographie",
+        dark_gray_canvas: "Fond gris foncé",
+        light_gray_canvas: "Fond gris pâle",
+        //national_geographic: "National Geographic",
+        oceans: "Océans",
+        terrain_with_labels: "Terrain avec étiquettes",
+        openstreetmap: "Plan d'accès",
+    })
+});
